5ada9d3bfee6b76ca7679f2383db68a31861671b
bchertok <boris.chertok@mail.ru>
2018-02-08 12:07:33 +0300

    тест по архивам красиво сделан

--- src/test/java/first/Archive.java
+++ src/test/java/first/Archive.java
@@ -1,14 +1,12 @@
 package first;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
-public class Test2 {
+public class Archive {
     private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
@@ -19,24 +17,41 @@ public class Test2 {
         driver = new FirefoxDriver();
         baseUrl = "http://sb-oad-test/app1/dictionaries/subdivisions";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        startPage();
+    }
+
+    private void startPage() {
+        driver.get("http://sb-oad-test/app1/dictionaries/subdivisions");
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("http://sb-oad-test/app1/dictionaries/subdivisions");
-        driver.findElement(By.xpath("//div[@id='root']/div/div[2]/div[2]/div[2]/div/div/div/div/a[17]/div")).click();
-        driver.findElement(By.linkText("Создать")).click();
-        driver.findElement(By.cssSelector("input.input")).click();
-        driver.findElement(By.cssSelector("input.input")).clear();
-        driver.findElement(By.cssSelector("input.input")).sendKeys("код");
-        driver.findElement(By.xpath("(//input[@value=''])[2]")).click();
-        driver.findElement(By.xpath("(//input[@value=''])[2]")).clear();
-        driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("наменование");
-        driver.findElement(By.xpath("//div[4]/div[2]/input")).click();
-        driver.findElement(By.xpath("//div[4]/div[2]/input")).clear();
-        driver.findElement(By.xpath("//div[4]/div[2]/input")).sendKeys("адрес");
+
+        newArchive();
+        fillArchive("код", "наменование", "адрес");
+        acceptArchive();
+    }
+
+    private void acceptArchive() {
         driver.findElement(By.cssSelector("div.p-content")).click();
         driver.findElement(By.cssSelector("button.g-button.success")).click();
+    }
+
+    private void fillArchive(String kod, String name, String adress) {
+        driver.findElement(By.cssSelector("input.input")).click();
+        driver.findElement(By.cssSelector("input.input")).clear();
+        driver.findElement(By.cssSelector("input.input")).sendKeys(kod);
+        driver.findElement(By.xpath("(//input[@value=''])[2]")).click();
+        driver.findElement(By.xpath("(//input[@value=''])[2]")).clear();
+        driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys(name);
+        driver.findElement(By.xpath("//div[4]/div[2]/input")).click();
+        driver.findElement(By.xpath("//div[4]/div[2]/input")).clear();
+        driver.findElement(By.xpath("//div[4]/div[2]/input")).sendKeys(adress);
+    }
+
+    private void newArchive() {
+        driver.findElement(By.xpath("//div[@id='root']/div/div[2]/div[2]/div[2]/div/div/div/div/a[17]/div")).click();
+        driver.findElement(By.linkText("Создать")).click();
     }
 
     @AfterClass(alwaysRun = true)
